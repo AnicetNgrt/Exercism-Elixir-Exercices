6a13d9eeb111c7f6ad3e3ffef192d02ee501ba38
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2020-12-08 12:22:29 +0100

    solved word-count exercice

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -6,5 +6,17 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    ignore_pattern = [",","!","?","&","@","$","%","^","_",":"]
+    String.replace(sentence, ignore_pattern, " ")
+      |> String.split(" ")
+      |> Enum.filter(fn word -> word != "" end)
+      |> Enum.reduce(%{}, fn word, acc ->
+          lowercase = String.downcase(word)
+          {word, val} = case acc do
+            %{^lowercase => val} -> {lowercase, val+1}
+            _ -> {lowercase, 1}
+          end
+          Map.put(acc, word, val)
+        end)
   end
 end
